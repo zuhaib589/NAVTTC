18ddb7c71ac1dbbb6be5be06f11148182d3ea379
zuhaib zafar <zuhaibzafar589@gmail.com>
2025-04-22 23:03:46 +0500

    .

--- Js/index.js
+++ Js/index.js
@@ -1,1 +1,18 @@
-let name="zuhaib"
+// let name = "zuhaib";
+
+// console.log();
+
+// // Arithmetic Operators + - * % / **
+
+// a = 5;
+// b = 6;
+
+
+// sum=a+b
+// console.log(sum);
+
+// let c = prompt("enter num");
+// console.log(c);
+
+let userInput = prompt("Enter your name:");
+console.log("Hello, " + userInput);
