00fb3e3e1d2e1422b87f5cda8f523d339a7940db
zuhaib zafar <zuhaibzafar589@gmail.com>
2025-04-23 21:41:29 +0500

    arithmetic

--- Js/index.js
+++ Js/index.js
@@ -4,15 +4,86 @@
 
 // // Arithmetic Operators + - * % / **
 
-// a = 5;
-// b = 6;
+a = 36;
+b = 5;
 
+// sum
 
 // sum=a+b
 // console.log(sum);
 
-// let c = prompt("enter num");
-// console.log(c);
+// sub 
 
-let userInput = prompt("Enter your name:");
-console.log("Hello, " + userInput);
+// sub=a-b
+// console.log(sub);  
+
+// mul 
+
+// mul=a*b
+// console.log(mul);
+
+// div
+
+// div=a/b
+// console.log(div);
+
+//expo
+
+// expo=a**b
+// console.log(expo);
+
+
+// mod 
+
+// mod=a%b
+// console.log(mod);
+
+// increment & decrement 
+
+// a += 10
+// console.log(a);
+// a++
+// console.log(a);
+
+// a--
+// console.log(a);
+
+
+
+
+// conditional statement 
+
+
+
+// LOOPS
+
+// FOR LOOP
+
+for(let i=0; i<10; i++)
+    console.log(i+1);
+
+// WHILE LOOP 
+
+// a=0
+// while(a<=5){
+//     console.log("hello jee");
+//     a++
+// }
+
+
+// DO while
+
+let i=1
+
+do{
+    console.log("zuhaib");
+    
+}
+while(i<0)
+    
+
+
+
+
+
+
